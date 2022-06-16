cd3b05f9161777e72813f57ce68ba41e5d1bb132
SeptemberHX <september_hx@outlook.com>
2022-06-16 14:47:20 +0800

    init

---
+++ src/codemirror/zettlr/index.ts
@@ -0,0 +1,78 @@
+// import './render-math'
+import CodeMirror from "codemirror";
+import {markdownRenderTasks} from "./render-tasks";
+import {foldCodeHelper} from "./foldcode-helper";
+import {markdownRenderTables} from "./render-tables";
+
+module.exports = {
+    default: function(_context) {
+        return {
+            plugin: function (CodeMirror) {
+                CodeMirror.defineOption("zettlrPlugins", [], async function(cm, val, old) {
+                    // While taskHandle is undefined, there's no task scheduled. Else, there is.
+                    let taskHandle: number|undefined
+
+                    const callback = function (cm: CodeMirror.Editor): void {
+                        if (taskHandle !== undefined) {
+                            return // Already a task registered
+                        }
+
+                        taskHandle = requestIdleCallback(function () {
+                            renderElements(cm)
+                            taskHandle = undefined // Next task can be scheduled now
+                        }, { timeout: 1000 }) // Don't wait more than 1 sec before executing this
+                    }
+
+                    cm.on('cursorActivity', callback)
+                    cm.on('viewportChange', callback) // renderElements)
+                    cm.on('optionChange', callback)
+                });
+
+                foldCodeHelper(CodeMirror);
+            },
+            codeMirrorOptions: {
+                'zettlrPlugins': true,
+                foldGutter: true,
+                gutters: ["CodeMirror-foldgutter"],
+                foldOptions: {
+                    'widget': '\u00A0\u2026\u00A0', // nbsp ellipse nbsp
+                    'scanUp': false // Do not search upwards if current line cannot be folded
+                },
+            },
+            codeMirrorResources: [
+                'addon/fold/foldcode',
+                'addon/fold/foldgutter',
+                'addon/fold/brace-fold',
+                'addon/fold/indent-fold',
+                'addon/fold/markdown-fold',
+                'addon/fold/comment-fold'
+            ],
+            assets: function() {
+                return [
+                    {
+                        name: 'zettlr.css'
+                    }
+                ];
+            }
+        }
+    },
+}
+
+function renderElements (cm: CodeMirror.Editor): void {
+    // const render = (cm as any).getOption('zettlr').render
+    // cm.execCommand('markdownRenderMermaid')
+    // cm.execCommand('clickableYAMLTags')
+    // if (render.tables === true) cm.execCommand('markdownRenderTables')
+    // if (render.iframes === true) cm.execCommand('markdownRenderIframes')
+    // if (render.links === true) cm.execCommand('markdownRenderLinks')
+    // if (render.images === true) cm.execCommand('markdownRenderImages')
+    // if (render.math === true) cm.execCommand('markdownRenderMath')
+    // if (render.citations === true) cm.execCommand('markdownRenderCitations')
+    // if (render.tasks === true) cm.execCommand('markdownRenderTasks')
+    // if (render.headingTags === true) cm.execCommand('markdownRenderHTags')
+    // if (render.emphasis === true) cm.execCommand('markdownRenderEmphasis')
+    console.log('=================> In renderElements');
+    // markdownRenderMath(cm);
+    markdownRenderTasks(cm);
+    markdownRenderTables(cm);
+}
